5b82b182a63a5eb7bbe5ae977e0994f47bdbe828
nmahajan-jpmc <89865781+nmahajan-jpmc@users.noreply.github.com>
2022-09-16 18:19:59 +0100

    moving sample-awesome-app to root

--- workshop/setup.d/init.sh
+++ workshop/setup.d/init.sh
@@ -4,6 +4,7 @@ npm config set prefix '/home/eduk8s/.npm-global'
 npm install -g solc
 npm install -g truffle
 export PATH=/home/eduk8s/.npm-global/bin:$PATH
+mv resources/sample-awesome-app ./sample-awesome-app
 cd resources/explorer
 npm i
 echo **init script called**
